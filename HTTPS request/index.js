9a55edf3cd3ba3f269d39c954410425f63e1ef74
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-11-18 18:58:09 +0530

    data sending ,endpoints, get function

--- HTTPS request/index.js
+++ HTTPS request/index.js
@@ -2,6 +2,17 @@ import express from 'express';
 const app = express();
 const port= 3000;
 
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello!</h1>")
+    
+})
+app.get("/Home",(req,res)=>{
+    res.send("<h1>Home!</h1>")
+    
+})
+app.get("/contact",(req,res)=>{
+    res.send("Contact Me!")
+})
 
 app.listen(port,()=>{
     console.log(`server started on port ${port}`);
