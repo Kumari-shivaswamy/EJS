0478aaaf487d5d7aeefd82b3f711c33531173af6
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-11-29 19:29:14 +0530

    logger function

--- Middlewares/index3.js
+++ Middlewares/index3.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+
+function logger(req, res, next) {
+  
+}
+
 app.use(logger);
 app.get("/", (req, res) => {
   res.send("Hello");
